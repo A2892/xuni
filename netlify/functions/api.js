398cb4d99e0b355e654613b117b9d163d2ae71f3
5201314ysys <2892411958@qq.com>
2026-05-07 16:05:24 +0800

    fix: 修复 Netlify Functions 中的 __filename 重复声明和 URL 路由问题

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,12 +1,16 @@
 import serverless from 'serverless-http';
 import app from '../../server.js';
 
-export const handler = serverless(app, {
+const handler = serverless(app, {
   request(req, event, context) {
+    // Fix the URL path for the Express app to find its routes correctly
     if (req.url && req.url.startsWith('/.netlify/functions/api')) {
-      req.url = req.url.replace('/.netlify/functions/api', '/api');
+      req.url = req.url.replace('/.netlify/functions/api', '');
     }
   }
 });
+
+export { handler };
+
 
 
